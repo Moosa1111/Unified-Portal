e3f1c86efc308b1561607fa9dee038d834cd5a3b
Moosa1111 <musa123ch@gmail.com>
2023-05-04 00:50:57 -0500

    latest commit

--- app/src/main/java/com/example/unifiedportal/HomeActivity.kt
+++ app/src/main/java/com/example/unifiedportal/HomeActivity.kt
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
-
-    //private lateinit var upcomingEventTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,63 +35,133 @@ class HomeActivity : AppCompatActivity() {
         val planner: Button = findViewById(R.id.planner)
         val chart: Button = findViewById(R.id.chart)
         val goals: Button = findViewById(R.id.GoalsButton)
-        val upcomingEventstext: TextView = findViewById(R.id.upcomingEventTextView)
+        val eventbutton : Button = findViewById(R.id.buttonviewevents)
+        val goalbutton : Button = findViewById(R.id.buttonviewgoals)
 
-//        upcomingEventTextView = findViewById(R.id.upcomingEventTextView)
-//
-        // Retrieve upcoming event from Firestore
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val userId = currentUser.uid
+        // Get the reference to the PieChart view
+        val barChart = findViewById<PieChart>(R.id.chart)
 
-            db.collection("users")
-                .document(userId)
-                .collection("events")
-                .get()
-                .addOnSuccessListener { result ->
-                    val upcomingEvents = mutableListOf<String>()
+        // Get the current user's ID
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
 
-                    for (document in result) {
-                        val event = document.toObject(Event::class.java)
-                        upcomingEvents.add(event.name)
-                    }
+        // Get the reference to the expenses subcollection
+        val expensesRef = db.collection("users").document(userId).collection("expenses")
 
-                    if (upcomingEvents.isNotEmpty()) {
-                        val upcomingEvents = upcomingEvents.first()
-                        upcomingEventstext.text = "Upcoming event: $upcomingEvents"
-                    } else {
-                        upcomingEventstext.text = "No upcoming events"
+        // Query the expenses subcollection and group the expenses by category
+        expensesRef
+            .get()
+            .addOnSuccessListener { result ->
+                val categories = hashMapOf<String, Double>()
+                for (document in result) {
+                    val category = document.getString("category") ?: ""
+                    val amount = document.getDouble("amount") ?: 0.0
+                    if (category.isNotEmpty()) {
+                        categories[category] = categories.getOrDefault(category, 0.0) + amount
                     }
                 }
-                .addOnFailureListener { exception ->
-                    upcomingEventstext.text = "Error retrieving upcoming events"
+
+                // Create a list of PieEntry objects representing the data to be displayed in the chart
+                val entries = arrayListOf<BarEntry>()
+                categories.forEachIndexed { index, (category, amount) ->
+                    entries.add(BarEntry(index.toFloat(), amount.toFloat(), category))
                 }
-        } else {
-            upcomingEventstext.text = "Please sign in to see upcoming events"
+                // Set the bar chart data and customize the appearance
+                val dataSet = BarDataSet(entries, "Expenses")
+                dataSet.colors = ColorTemplate.COLORFUL_COLORS.asList()
+                val data = BarData(dataSet)
+                chart.data = data
+                chart.setFitBars(true)
+                chart.setDrawValueAboveBar(true)
+                chart.description.isEnabled = false
+                chart.legend.isEnabled = false
+                chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+                chart.xAxis.labelCount = entries.size
+                chart.xAxis.valueFormatter = object : ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        return entries[value.toInt()].data as String
+                    }
+                }
+
+                // Redraw the chart
+                chart.invalidate()
+            }
+
+// Add a listener to the Firestore expenses collection to retrieve the expenses data
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        val expensesRef = db.collection("users").document(userId).collection("expenses")
+        expensesRef.addSnapshotListener { value, error ->
+            if (error != null) {
+                Toast.makeText(this, "Error getting expenses: ${error.message}", Toast.LENGTH_SHORT).show()
+            } else {
+                val expensesData = value!!.documents
+                    .map { it["category"] as String to (it["amount"] as Double) }
+                    .groupBy { it.first }
+                    .mapValues { it.value.sumOf { (_, amount) -> amount } }
+                updateBarChart(expensesData)
+            }
         }
 
-        profile.setOnClickListener{
+// Update the bar chart whenever a new expense is added
+// Add the following code inside the `addOnSuccessListener` block in your `ExpenditureActivity` class
+        val homeActivityIntent = Intent(this, HomeActivity::class.java)
+        startActivity(homeActivityIntent)
+        finish()
+
+        profile.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
 
-        expenditure.setOnClickListener{
+        expenditure.setOnClickListener {
             val intent = Intent(this, ExpenditureActivity::class.java)
             startActivity(intent)
         }
 
-        planner.setOnClickListener{
+        planner.setOnClickListener {
             val intent = Intent(this, PlannerActivity::class.java)
             startActivity(intent)
         }
 
-        chart.setOnClickListener{
+        chart.setOnClickListener {
             val intent = Intent(this, ChartActivity::class.java)
             startActivity(intent)
         }
-        goals.setOnClickListener{
+        goals.setOnClickListener {
             val intent = Intent(this, AddGoalActivity::class.java)
             startActivity(intent)
         }
-    }
+
+        goalbutton.setOnClickListener {
+            val intent = Intent(this, AllGoalsActivity::class.java)
+            startActivity(intent)
+        }
+
+        eventbutton.setOnClickListener {
+            val intent = Intent(this, AllEventsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+//        // Set the bar chart data and customize the appearance
+//        val dataSet = BarDataSet(entries, "Expenses")
+//        dataSet.colors = ColorTemplate.COLORFUL_COLORS.asList()
+//        val data = BarData(dataSet)
+//        chart.data = data
+//        chart.setFitBars(true)
+//        chart.setDrawValueAboveBar(true)
+//        chart.description.isEnabled = false
+//        chart.legend.isEnabled = false
+//        chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+//        chart.xAxis.labelCount = entries.size
+//        chart.xAxis.valueFormatter = object : ValueFormatter() {
+//            override fun getFormattedValue(value: Float): String {
+//                return entries[value.toInt()].data as String
+//            }
+//        }
+//
+//        // Redraw the chart
+//        chart.invalidate()
+//    }
+
 }
